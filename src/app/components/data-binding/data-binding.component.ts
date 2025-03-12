4965d9b431357e4dc017353af529df2e60a156df
Danish Ali <dali27363@gmail.com>
2025-03-12 11:27:45 +0500

    one way binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
+  courseName: string = "Python Crash course"
+  courseDuration: string = "3 hours"
+  myPlaceholder = "Enter course name"
+  myClass = "bg-success text-light"
 
+  showAlert() {
+    alert("Welcome to the course of angular 19")
+  }
+
+  showMsg(msg: string) {
+    alert(msg);
+  }
+
+  onStateChange(){
+    alert("State got changed")
+  }
 }
