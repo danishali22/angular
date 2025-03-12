a2461016b74d67603fdefa570989ecb1cee0c35b
Danish Ali <dali27363@gmail.com>
2025-03-12 10:50:47 +0500

    Add Bootstrap for styling and enhance data-binding component layout

--- src/app/components/variables/variables.component.ts
+++ src/app/components/variables/variables.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './variables.component.css'
 })
 export class VariablesComponent {
+  firstName: string = "Danish";
+  lastName = "Ali";
+  age: number = 24;
+  isActive: boolean = true;
+  courseName: string;
+  joining: Date = new Date();
+  courseDuartion: any;
 
+  constructor(){
+    this.courseName = "Python";
+    this.courseDuartion = "30 mnts";
+    this.courseDuartion = false;
+  }
 }
