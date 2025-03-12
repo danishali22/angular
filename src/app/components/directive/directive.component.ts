4264d936e2f889cb00816db1ac3db10a56d806f8
Danish Ali <dali27363@gmail.com>
2025-03-12 12:57:45 +0500

    ng for

--- src/app/components/directive/directive.component.ts
+++ src/app/components/directive/directive.component.ts
@@ -1,10 +1,10 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directive',
-  imports: [NgIf, FormsModule],
+  imports: [NgIf, FormsModule, NgFor],
   templateUrl: './directive.component.html',
   styleUrl: './directive.component.css'
 })
@@ -14,6 +14,21 @@ export class DirectiveComponent {
   num1: number = 0;
   num2: number = 0;
   selectedCast: string = "";
+
+  cityList: string[] = ["Lahore", "Karachi", "Peshawar", "Quetta", "Multan"];
+
+  studentList: {
+    name: string;
+    city: string;
+    grade: string;
+    isActive: boolean;
+  }[] = [
+      { name: "Danish", city: "Lahore", grade: 'A', isActive: true},
+      { name: "Amir", city: "Karachi", grade: '', isActive: true},
+      { name: "Javaid", city: "Peshawar", grade: 'B', isActive: false},
+      { name: "Haider", city: "Quetta", grade: 'C', isActive: true},
+      { name: "Aqib", city: "Multan", grade: 'B', isActive: false},
+  ]
 
   showDiv1(){
     this.isDiv1Visible = true;
